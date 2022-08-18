26e8a4eca5860ad97b99eca1aef187ccbf85d290
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-18 13:27:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+var divide = (num1 , num2) => {
+    num1 =2000;
+    num2 =100;
+    return num1 / num2;
+  }
+
+  divide(2000,100)
+
+
+  var square = (x) => {
+     x =2;
+    return x * x ;
+    }
+    square(2)
+
+    var add  = (a, b) => {
+        a =3;
+        b =4;
+        return a + b;
+    }
+     add(a,b)
+     
